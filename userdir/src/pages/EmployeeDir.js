89415cb8bb825298c4dd510d5afae2b81c06f1f6
Mario Cortes <mcortes.1113@gmail.com>
2020-01-15 23:48:50 -0600

    Added content to page

--- userdir/src/pages/EmployeeDir.js
+++ userdir/src/pages/EmployeeDir.js
@@ -1,13 +1,60 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import DirImg from "./assets/directory.png";
 
 function Home() {
     return (
         <div>
             <NavBar />
-            <h1 class="text-center">Directory Placeholder</h1>
-            <h3 class="text-center">Placeholder</h3>
+                <Container>
+                    <Row>
+                    {/* <Col size="md-2"> */}
+                    <Col size="md-2">
+                        <img
+                            src={DirImg}
+                            alt="directory"
+                            width="75px"
+                            height="75px"
+                        />
+                    </Col>
+                        <Col size="md-10">
+                            <h1>Employee Directory</h1>
+                        </Col>
+                    </Row>
+                        <Row>
+                            <Col size="md-2">
+                                <p>Sort By: </p>
+                            </Col>
+                                <Col size="md-3">
+                                    <p>Dropdown placeholder</p>
+                                </Col>
+                                    <Col size="md-7">
+                                        <p>Sort button placeholder</p>
+                                    </Col>
+                        </Row>
+                            <Row>
+                                <Col size="md-2">
+                                    <p>Filter By: </p>
+                                </Col>
+                                    <Col size="md-3">
+                                        <p>Dropdown placeholder</p>
+                                    </Col>
+                                        <Col size="md-7">
+                                            <p>Filter button placeholder</p>
+                                        </Col>
+                            </Row>
+                                <Row>
+                                    <Col size="md-12">
+                                        <h3>List of Cards will go here...</h3>
+                                    </Col>
+                                </Row>
+                </Container>
+            {/* <h1 class="text-center">Directory Placeholder</h1>
+            <h3 class="text-center">Placeholder</h3> */}
         </div>
     )
 }
