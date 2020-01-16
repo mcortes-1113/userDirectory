6b90b43c12792c6f1eb7f6c6549b521f1812158c
Mario Cortes <mcortes.1113@gmail.com>
2020-01-15 23:19:39 -0600

    Updated file structure

--- userdir/src/pages/EmployeeDir.js
+++ userdir/src/pages/EmployeeDir.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Home() {
     return (
         <div>
+            <NavBar />
             <h1 class="text-center">Directory Placeholder</h1>
             <h3 class="text-center">Placeholder</h3>
         </div>
