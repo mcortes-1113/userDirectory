3bd1f32934665292d2f873345450dbf44608deb5
Mario Cortes <mcortes.1113@gmail.com>
2020-01-15 21:31:57 -0600

    Created Home component

---
+++ userdir/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div>
+            <h1 class="text-center">Welcome</h1>
+            <h3 class="text-center">Please use the link on the top of the page to navigate to the Directory page</h3>
+        </div>
+    )
+}
+
+export default Home;
