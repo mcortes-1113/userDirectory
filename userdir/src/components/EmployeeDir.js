5451460ba7c843f550d3bbdb538cb363f6659d79
Mario Cortes <mcortes.1113@gmail.com>
2020-01-15 21:39:45 -0600

    Created EmployeeDir component as placeholder

---
+++ userdir/src/components/EmployeeDir.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div>
+            <h1 class="text-center">Directory Placeholder</h1>
+            <h3 class="text-center">Placeholder</h3>
+        </div>
+    )
+}
+
+export default Home;
