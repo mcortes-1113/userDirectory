e41215f8eda840fd6cdbd1ca7137397328e0447b
Mario Cortes <mcortes.1113@gmail.com>
2020-01-17 23:22:54 -0600

    Replaced dropdown buttons with regular buttons

--- userdir/src/components/Filter/index.js
+++ userdir/src/components/Filter/index.js
@@ -3,16 +3,9 @@ import React from "react";
 function FilterBy() {
 
   return (
-    <div className="dropdown">
-      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Filter By
-      </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item">Show All</a>
-            <a className="dropdown-item">Department</a>
-            <a className="dropdown-item">Active</a>
-            <a className="dropdown-item">Inactive</a>
-        </div>
+    <div>
+    <button type="button" className="btn btn-light">Filter by Department</button>
+    <button type="button" className="btn btn-light">Filter by Active Status</button>
     </div>
   )
 }
--- userdir/src/components/Sort/index.js
+++ userdir/src/components/Sort/index.js
@@ -3,16 +3,9 @@ import React from "react";
 function SortBy() {
 
   return (
-    <div className="dropdown">
-      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Sort By
-      </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item">First Name</a>
-            <a className="dropdown-item">Last Name</a>
-            {/* <a className="dropdown-item" href="#">First Name</a>
-            <a className="dropdown-item" href="#">Last Name</a> */}
-        </div>
+    <div>
+    <button type="button" className="btn btn-info">Sort by First Name</button>
+    <button type="button" className="btn btn-info">Sort by Last Name</button>
     </div>
   )
 }
