eb1e149fbb36e136c447eb249dd32bd01a420cd9
Mario Cortes <mcortes.1113@gmail.com>
2020-01-20 20:14:20 -0600

    Added functions to filter and sort employees

--- userdir/src/components/Sort/index.js
+++ userdir/src/components/Sort/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Employees from "../../Employees.json";
 
 function SortBy() {
 
+  function sort() {
+    console.log("sort");
+    const sorted = [].concat(Employees) 
+      .sort((a, b) => (a.lastName > b.lastName) ? 1 : -1);
+      console.log(sorted);
+  }
+
   return (
     <div>
-    <button type="button" className="btn btn-info">Sort by First Name</button>
-    <button type="button" className="btn btn-info">Sort by Last Name</button>
+    {/* <button type="button" className="btn btn-info" onClick</div>)>Sort by First Name</button> */}
+    <button type="button" className="btn btn-info" onClick = { sort } >Sort by Last Name</button>
     </div>
   )
 }
